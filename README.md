c946af2d2af66d3ca9759ccfcd96a852bb54b7ef
Lucas Pickering <lucas@lucaspickering.me>
2024-08-15 10:10:45 -0400

    Rename to env-lock

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
-# env-guard
+# env-lock
 
-[![Test CI](https://github.com/github/docs/actions/workflows/test.yml/badge.svg)](https://github.com/LucasPickering/env-guard/actions)
-[![crates.io](https://img.shields.io/crates/v/env-guard.svg)](https://crates.io/crates/env-guard)
-[![docs.rs](https://img.shields.io/docsrs/env-guard)](https://docs.rs/env-guard)
+[![Test CI](https://github.com/github/docs/actions/workflows/test.yml/badge.svg)](https://github.com/LucasPickering/env-lock/actions)
+[![crates.io](https://img.shields.io/crates/v/env-lock.svg)](https://crates.io/crates/env-lock)
+[![docs.rs](https://img.shields.io/docsrs/env-lock)](https://docs.rs/env-lock)
 
-A process's environment is a form of global mutable state. In Rust, tests are run in a shared process. This means tests that modify environment variables can inadvertently affect each other. `env-guard` provides an interface to safely modify and lock the process environment, to prevent simultaneous access.
+A process's environment is a form of global mutable state. In Rust, tests are run in a shared process. This means tests that modify environment variables can inadvertently affect each other. `env-lock` provides an interface to safely modify and lock the process environment, to prevent simultaneous access.
 
 ```rust
-use env_guard::EnvGuard;
 use std::env;
 
-let var = "ENV_GUARD_TEST_VARIABLE";
+let var = "ENV_LOCK_TEST_VARIABLE";
 assert!(env::var(var).is_err());
 
-let guard = EnvGuard::lock([(var, Some("hello!"))]);
+let guard = env_lock::lock_env([(var, Some("hello!"))]);
 assert_eq!(env::var(var).unwrap(), "hello!");
 drop(guard);
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,17 @@
-//! Lock environment variables to prevent simultaneous access. Use the
-//! [EnvGuard] to set values for whatever environment variables you intend to
-//! access in your test. This will return a guard that, when dropped, will
-//! revert the environment to its initial state. The guard uses a
-//! [Mutex] underneath to ensure that multiple tests within the same process
-//! can't access it at the same time.
-//!
-//! > Note: There is a single mutex per process that locks the *entire*
-//! environment. This means any code locked by `EnvGuard` cannot run
-//! concurrently, even if they don't modify any of the same environment
-//! variables. Keep your critical sections as short as possible to prevent
-//! slowdowns.
+//! Lock environment variables to prevent simultaneous access. Use [lock_env] to
+//! set values for whatever environment variables you intend to access in your
+//! test. This will return a guard that, when dropped, will revert the
+//! environment to its initial state. The guard uses a [Mutex] underneath to
+//! ensure that multiple tests within the same process can't access it at the
+//! same time.
 //!
 //! ```
-//! use env_guard::EnvGuard;
 //! use std::env;
 //!
-//! let var = "ENV_GUARD_TEST_VARIABLE";
+//! let var = "ENV_LOCK_TEST_VARIABLE";
 //! assert!(env::var(var).is_err());
 //!
-//! let guard = EnvGuard::lock([(var, Some("hello!"))]);
+//! let guard = env_lock::lock_env([(var, Some("hello!"))]);
 //! assert_eq!(env::var(var).unwrap(), "hello!");
 //! drop(guard);
 //!
@@ -33,51 +26,56 @@ use std::{
     sync::{Mutex, MutexGuard},
 };
 
+/// Lock the environment and set each given variable to its corresponding
+/// value. If the environment is already locked, this will block until the lock
+/// can be acquired. The returned guard will keep the environment locked so the
+/// calling test has exclusive access to it. Upon being dropped, the old
+/// environment values will be restored and then the environment will be
+/// unlocked.
+///
+/// ## Note
+/// There is a single mutex per process that locks the *entire*
+/// environment. This means multiple usages of by `lock_env` cannot run
+/// concurrently, even if they don't modify any of the same environment
+/// variables. Keep your critical sections as short as possible to prevent
+/// slowdowns.
+pub fn lock_env<'a>(
+    variables: impl IntoIterator<Item = (&'a str, Option<impl AsRef<str>>)>,
+) -> EnvGuard<'a> {
+    /// Global mutex for accessing environment variables. Technically we
+    /// could break this out into a map with one mutex per variable, but
+    /// that adds a ton of complexity for very little value.
+    static ENV_MUTEX: Mutex<()> = Mutex::new(());
+
+    let guard = ENV_MUTEX.lock().expect("Environment lock is poisoned");
+    let previous_values = variables
+        .into_iter()
+        .map(|(variable, new_value)| {
+            let previous_value = env::var(variable).ok();
+
+            if let Some(value) = new_value {
+                env::set_var(variable, value.as_ref());
+            } else {
+                env::remove_var(variable);
+            }
+
+            (variable, previous_value)
+        })
+        .collect();
+
+    EnvGuard {
+        previous_values,
+        guard,
+    }
+}
+
 /// A guard used to indicate that the current process environment is locked.
-/// This should be used in all tests that access environment variables, to
-/// prevent interference from external variable settings or tests conflicting
-/// with each other.
+/// Returned by [lock_env]. This will restore and unlock the environment on
+/// drop.
 pub struct EnvGuard<'a> {
     previous_values: Vec<(&'a str, Option<String>)>,
     #[allow(unused)]
     guard: MutexGuard<'static, ()>,
-}
-
-impl<'a> EnvGuard<'a> {
-    /// Lock the environment and set each given variable to its corresponding
-    /// value. The returned guard will keep the environment locked so the
-    /// calling test has exclusive access to it. Upon being dropped, the old
-    /// environment values will be restored and then the environment will be
-    /// unlocked.
-    pub fn lock(
-        variables: impl IntoIterator<Item = (&'a str, Option<impl AsRef<str>>)>,
-    ) -> Self {
-        /// Global mutex for accessing environment variables. Technically we
-        /// could break this out into a map with one mutex per variable, but
-        /// that adds a ton of complexity for very little value.
-        static MUTEX: Mutex<()> = Mutex::new(());
-
-        let guard = MUTEX.lock().expect("Environment lock is poisoned");
-        let previous_values = variables
-            .into_iter()
-            .map(|(variable, new_value)| {
-                let previous_value = env::var(variable).ok();
-
-                if let Some(value) = new_value {
-                    env::set_var(variable, value.as_ref());
-                } else {
-                    env::remove_var(variable);
-                }
-
-                (variable, previous_value)
-            })
-            .collect();
-
-        Self {
-            previous_values,
-            guard,
-        }
-    }
 }
 
 impl<'a> Drop for EnvGuard<'a> {
@@ -98,16 +96,16 @@ mod tests {
     use super::*;
 
     // NOTE: Because these tests specifically modify environment variables
-    // *outside* the env guard, they each need to use a different variable. If
+    // *outside* the env lock, they each need to use a different variable. If
     // only someone make a library that would avoid that...
 
     /// Set a value for a variable that doesn't exist yet
     #[test]
     fn set_missing_var() {
-        let var = "ENV_GUARD_TEST_VARIABLE_SET_MISSING";
+        let var = "ENV_LOCK_TEST_VARIABLE_SET_MISSING";
         assert!(env::var(var).is_err());
 
-        let guard = EnvGuard::lock([(var, Some("hello!"))]);
+        let guard = lock_env([(var, Some("hello!"))]);
         assert_eq!(env::var(var).unwrap(), "hello!");
         drop(guard);
 
@@ -117,11 +115,11 @@ mod tests {
     /// Override the value for a preexisting variable
     #[test]
     fn set_existing_var() {
-        let var = "ENV_GUARD_TEST_VARIABLE_SET_EXISTING";
+        let var = "ENV_LOCK_TEST_VARIABLE_SET_EXISTING";
         env::set_var(var, "existing");
         assert_eq!(env::var(var).unwrap(), "existing");
 
-        let guard = EnvGuard::lock([(var, Some("hello!"))]);
+        let guard = lock_env([(var, Some("hello!"))]);
         assert_eq!(env::var(var).unwrap(), "hello!");
         drop(guard);
 
@@ -131,11 +129,11 @@ mod tests {
     /// Remove the value for a preexisting variable
     #[test]
     fn clear_existing_var() {
-        let var = "ENV_GUARD_TEST_VARIABLE_CLEAR_EXISTING";
+        let var = "ENV_LOCK_TEST_VARIABLE_CLEAR_EXISTING";
         env::set_var(var, "existing");
         assert_eq!(env::var(var).unwrap(), "existing");
 
-        let guard = EnvGuard::lock([(var, None::<&str>)]);
+        let guard = lock_env([(var, None::<&str>)]);
         assert!(env::var(var).is_err());
         drop(guard);
 
